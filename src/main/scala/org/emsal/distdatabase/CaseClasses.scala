97f23fe946fd63da3c08392e4b098065a9e3649d
Em Sal <absolutezero2a03@gmail.com>
2016-04-08 12:50:12 -0700

    did some stuff for a basic implementation of retrieving data with ids

--- src/main/scala/org/emsal/distdatabase/CaseClasses.scala
+++ src/main/scala/org/emsal/distdatabase/CaseClasses.scala
@@ -1,8 +1,13 @@
 package org.emsal.distdatabase
 
+import akka.actor.ActorPath
+
 /**
   * Created by em on 4/1/16.
   */
 
 case class StoreData(data: String)
 case object DataWritten
+case class GetData(id: String)
+case class FoundData(actorPath: ActorPath, data:String)
+case class NothingFoundHere(actorPath: ActorPath)
--- src/main/scala/org/emsal/distdatabase/Storer.scala
+++ src/main/scala/org/emsal/distdatabase/Storer.scala
@@ -10,6 +10,7 @@ import better.files._
   * Created by em on 3/31/16.
   */
 class Storer extends Actor with ActorLogging {
+  import context._
 
   val cluster = Cluster(context.system)
   val conf = ConfigFactory.load()
@@ -20,16 +21,44 @@ class Storer extends Actor with ActorLogging {
   override def preStart(): Unit = cluster.subscribe(self, classOf[MemberEvent], classOf[UnreachableMember])
   override def postStop(): Unit = cluster.unsubscribe(self)
 
+  def locked: Receive = {
+    case StoreData(data) => {}
+    case GetData(id) => {}
+    case _: MemberEvent => {}
+  }
+
   def receive = {
     case StoreData(data) => storeData(data)
-    case DataWritten => lock()
+    case DataWritten => become(locked)
     case MemberUp(member) => {}
+    case GetData(id) => retrieveData(id)
     case UnreachableMember(member) => {}
     case MemberRemoved(member, previousStatus) => {}
     case _: MemberEvent => {}
   }
 
   def lock(): Unit = {}
+
+  def retrieveData(id: String): Unit = {
+    val outputFile: File = File(outputFilename)
+    val fileContents: String = outputFile.contentAsString
+    val lines: List[String] = fileContents.split("\n").toList
+
+    val entries: List[(String, String)] = lines.map((line: String) => {
+      val splitLine: List[String] = lines.split(",").map((x: String) => x.trim)
+
+      (splitLine(0), splitLine(1))
+    })
+
+    val entriesMap: Map[String, String] = entries.map(i => i._1 -> i._2).toMap
+
+    entriesMap.get(id) match {
+      case Some(data) => sender() ! FoundData(self.path, data)
+      case None => sender() ! NothingFoundHere(self.path)
+    }
+  }
+
+
 
 
   def storeData(data: String): Unit = {
